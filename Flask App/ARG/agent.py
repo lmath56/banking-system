82378b50a3698c8f220155bd591d37a7cad58fdc
Mathews,Lucas L <l.mathews@student.fontys.nl>
2024-05-28 11:25:17 +0000

    Delete unused folders and files

--- Flask App/ARG/agent.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--x', type=int, required=True)
-parser.add_argument('--y', type=int, required=True)
-args = parser.parse_args()
-product = args.x * args.y
-print(f"{args.x} * {args.y} = {product}")
